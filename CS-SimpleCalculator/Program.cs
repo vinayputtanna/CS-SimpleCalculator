a4a8eb90b50f4a2dbb80f236aec68c86d7ac53bd
Vinay Puttanna <vinay.puttanna@sjsu.edu>
2017-09-18 17:33:32 -0700

    Added basic arithmetic operations

--- CS-SimpleCalculator/Program.cs
+++ CS-SimpleCalculator/Program.cs
@@ -10,6 +10,35 @@ namespace CS_SimpleCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Simple Calculator");
+            Console.WriteLine("Enter the first operand:");
+            int firstOperand = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the operator");
+            string op=Console.ReadLine();
+            Console.WriteLine("Enter the second operator");
+            int secondOperand = int.Parse(Console.ReadLine());
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(string.Format("The sum of {0} and {1} is {2}", firstOperand, secondOperand,(firstOperand + secondOperand)));
+                    break;
+                case "-":
+                    Console.WriteLine(string.Format("The difference of {0} and {1} is {2}", firstOperand, secondOperand, (firstOperand - secondOperand)));
+                    break;
+                case "*":
+                    Console.WriteLine(string.Format("The product of {0} and {1} is {2}", firstOperand, secondOperand, (firstOperand * secondOperand)));
+                    break;
+                case "/":
+                    Console.WriteLine(string.Format("The quotient of {0} and {1} is {2}", firstOperand, secondOperand, (firstOperand / secondOperand)));
+                    break;
+                case "%":
+                    Console.WriteLine(string.Format("The remainder of {0} and {1} is {2}", firstOperand, secondOperand, (firstOperand % secondOperand)));
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
